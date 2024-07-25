cb305f50545914dc75e091264eeb4248ec67c58c
hai <115059867+hackerluongsuA@users.noreply.github.com>
2024-07-25 18:37:00 +0700

    Fix: fix text button

--- code_fe_do_an/src/components/course/CourseItem.tsx
+++ code_fe_do_an/src/components/course/CourseItem.tsx
@@ -67,12 +67,12 @@ const CourseItem: React.FC<CourseResponse> = (course: CourseResponse) => {
 
   return (
     <div className="flex flex-col items-center w-full h-full gap-3">
-      <div className="w-full h-full bg-[#b6da9f] rounded-2xl justify-around flex flex-row py-3 items-center">
+      <div className={`w-full h-full bg-[#b6da9f] rounded-2xl justify-around flex flex-row py-3 items-center ${isEnrolled ? 'bg-[#68ccd9]' : 'bg-[#b6da9f]'}`}>
         <img className="w-1/4 h-full rounded-2xl" src={course.course_image} alt={course.course_name} />
         <div className="flex flex-col w-1/2 h-full gap-5 p-4">
           <div className="font-semibold text-2xl text-green-700">{course.course_name}</div>
           <div className="text-xs">{course.description}</div>
-          {isEnrolled && course.totalProgress && (
+          {isEnrolled && course.totalProgress !== 0 && (
             <div className="flex flex-row items-center gap-3">
               <Progress className="h-[10px] basis-11/12" value={Math.round(course.totalProgress)} />
               <div className="basis-1/12">{Math.round(course.totalProgress)}%</div>
@@ -80,7 +80,7 @@ const CourseItem: React.FC<CourseResponse> = (course: CourseResponse) => {
           )}
         </div>
         <Button onClick={handleEnroll}>
-          {isEnrolled ? "Continue" : "Enroll"}
+          {isEnrolled ? "Tiếp tục" : "Đăng ký"}
         </Button>
       </div>
       <Modal
@@ -115,7 +115,7 @@ const CourseItem: React.FC<CourseResponse> = (course: CourseResponse) => {
           <img className="w-full h-auto rounded-2xl" src={course.course_image} alt={course.course_name} />
           <div className="mt-4 flex flex-col items-center gap-2">
             <Button className="bg-green-600 text-white font-bold py-4 px-6 rounded hover:bg-green-700 flex flex-col items-center p-6" onClick={handleConfirmEnroll}>
-              <span className="text-lg font-bold leading-none">Enroll</span>
+              <span className="text-lg font-bold leading-none">Đăng ký</span>
             </Button>
           </div>
         </div>
