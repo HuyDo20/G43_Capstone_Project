24cca5c7749f70d67d3b2b168ebabc542e2a4487
hai <87465508+HuyDo20@users.noreply.github.com>
2024-08-23 00:51:07 +0700

    Fix: null exam id

--- code_fe_do_an/src/components/course/DaySchedule.tsx
+++ code_fe_do_an/src/components/course/DaySchedule.tsx
@@ -87,8 +87,15 @@ export default function DaySchedule({ weekSelected, id = null }) {
         headers: { Authorization: token },
       });
       const responseExam = requestExam.data;
-      setWeeklyExamId(responseExam.statusCode === 200 ? responseExam.data.data.exam_id : 0);
-      await fetchExamHistories();
+      if (responseExam.statusCode === 200) {
+        console.log(responseExam);
+        if (responseExam.data.data?.exam_id) {
+          setWeeklyExamId(responseExam.data.data.exam_id);
+        } else {
+          setWeeklyExamId(0);
+        }
+          await fetchExamHistories();
+      }
     }
   };
   
